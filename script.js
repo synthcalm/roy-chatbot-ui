f2af0b75c48e0be9a1befc15cafdc8fdd31f39a7
synthcalm <synthcalm@gmail.com>
2025-04-14 02:45:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let thinkingEl = null;
 
     const ASSEMBLYAI_SOCKET_URL = 'wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000';
-    const ASSEMBLYAI_API_KEY = 'YOUR_ASSEMBLYAI_API_KEY';
+    const ASSEMBLYAI_API_KEY = 'eeee5d1982444610a670bd17152a8e4a';
     let socket;
 
     // Display greeting on load
